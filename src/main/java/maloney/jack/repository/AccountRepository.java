a90b6a9e5ad94c05836a7b8089798016f17dadef
jackmaloney <jack.maloney@transformuk.com>
2018-04-28 14:40:03 +0100

    Create an interface extending CrudRepository

---
+++ src/main/java/maloney/jack/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package maloney.jack.repository;
+
+import maloney.jack.domain.Account;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, String> {
+
+    @Override
+    String save(Account account);
+
+}
